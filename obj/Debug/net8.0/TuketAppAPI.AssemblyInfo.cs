3105c3f83d13fbf610e8c7820a8224e2692dac33
Berke Seker <berkeseker.2000@gmail.com>
2025-02-22 23:38:25 +0300

    Api bağlantısı sağlandı, giriş, kayıt olma ve anasayfa kodları eklendi

--- obj/Debug/net8.0/TuketAppAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/TuketAppAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TuketAppAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fe84b8268465a887b391278d5768a47054c3088")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e25329e82d1e4f92e61a03d0beaac9184913b1d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("TuketAppAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TuketAppAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
